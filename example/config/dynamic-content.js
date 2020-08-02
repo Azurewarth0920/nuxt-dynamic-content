bfdc602e9bf7c6865cac03ce04c2b6544dc3cf0f
pei.fan <fanpei920@gmail.com>
2020-08-02 17:06:05 +0900

    added example from reading from resources

--- example/config/dynamic-content.js
+++ example/config/dynamic-content.js
@@ -1,30 +1,35 @@
 const { resolve } = require("path");
+const { readFileSync } = require("fs");
 
-module.exports = {
-  routesList: [
-    {
-      path: '/detail/1',
-      componentPath: resolve(__dirname, '../dynamic-template/detail.vue')
-    },
-    {
-      path: '/detail/2',
-      componentPath: resolve(__dirname, '../dynamic-template/detail.vue')
-    },
-    {
-      path: '/year/2019',
-      componentPath: resolve(__dirname, '../dynamic-template/year.vue')
-    },
-    {
-      path: '/year/2020',
-      componentPath: resolve(__dirname, '../dynamic-template/year.vue')
-    },
-    {
-      path: '/category/news',
-      componentPath: resolve(__dirname, '../dynamic-template/category.vue')
-    },
-    {
-      path: '/category/entertainment',
-      componentPath: resolve(__dirname, '../dynamic-template/category.vue')
-    },
+module.exports = async function() {
+  const { data } = JSON.parse(readFileSync(resolve(__dirname, '../dynamic-resources/list.json')))
+
+  const yearList = Array.from(new Set(data.map(item => new Date(item.published_at).getFullYear())))
+    .map(yearItem => ({
+      path: `/year/${yearItem}`,
+      componentPath: resolve(__dirname, '../dynamic-template/year.vue'),
+      locals: yearItem
+    }))
+  
+  const categoryList = Array.from(new Set(data.map(item => item.category)))
+    .map(categoryItem => ({
+      path: `/category/${categoryItem}`,
+      componentPath: resolve(__dirname, '../dynamic-template/category.vue'),
+      locals: categoryItem
+    }))
+  
+  const detailList = Array.from(new Set(data.map(item => item.id)))
+    .map(detailId => ({
+      path: `/detail/${detailId}`,
+      componentPath: resolve(__dirname, '../dynamic-template/detail.vue'),
+      locals: JSON.parse(readFileSync(resolve(__dirname, `../dynamic-resources/feed_${detailId}.json`)))
+    }))
+
+  const routesList = [
+    ...detailList,
+    ...yearList,
+    ...categoryList
   ]
+
+  return { routesList, globals: data }
 }
