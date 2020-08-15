ddc44ff4ecb530044ce4636ff2ca9043fcecf96d
pei.fan <fanpei920@gmail.com>
2020-08-15 16:29:33 +0900

    refactoring

--- example/config/dynamic-content.js
+++ example/config/dynamic-content.js
@@ -2,37 +2,31 @@ const { resolve } = require('path')
 const { readFileSync } = require('fs')
 const contentBuilder = require('../../lib/content-helper')
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 module.exports = async function() {
   const { data } = JSON.parse(
     readFileSync(resolve(__dirname, '../dynamic-resources/list.json'))
   )
-
-  const yearList = contentBuilder(data, item => new Date(item.published_at), {
-    path: item => `/date/${item}`,
-    component: '~/dynamic-template/year.vue'
-  })
-
+  const yearList = contentBuilder(
+    data,
+    item => new Date(item.published_at).getFullYear(),
+    {
+      path: item => `/year/${item}`,
+      component: '~/dynamic-template/year.vue'
+    }
+  )
   const categoryList = contentBuilder(data, item => item.category, {
     path: category => `/category/${category}`,
     component: '~/dynamic-template/category.vue'
   })
-
   const detailList = await contentBuilder(data, item => item.id, {
     path: detailId => `/detail/${detailId}`,
     component: '~/dynamic-template/detail.vue',
-    resource: async detailId => {
-      await sleep(1000)
+    resource: detailId => {
       return readFileSync(
         resolve(__dirname, `../dynamic-resources/feed_${detailId}.json`)
       )
     }
   })
-
   const modules = [detailList, yearList, categoryList]
-
   return { modules, globals: data }
 }
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  collectCoverage: true,
-  collectCoverageFrom: ['lib/**/*.js'],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/lib/$1'
   },
