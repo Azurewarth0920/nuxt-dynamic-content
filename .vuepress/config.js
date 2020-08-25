ca29fdaf632566e4b3e97b9402c8a1fd6a1ae939
pei.fan <fanpei920@gmail.com>
2020-08-25 23:05:45 +0900

    change base dir

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,15 @@
 module.exports = {
   title: 'Nuxt dynamic content',
   description: 'Generate nuxt static site with dynamic resources',
-  ga: '',
+  base: '/nuxt-dynamic-content/',
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-176277165-1',
+      },
+    ],
+  ],
   dest: 'docs',
   themeConfig: {
     repo: 'Azurewarth0920/nuxt-dynamic-content',
